1311b2ede2cd42d2bda9e606fd95d7215ccb0490
Bill G <gbilly3@gmail.com>
2019-02-21 22:59:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
+
 # Add  code here!
+
+def prime?(int)
+  
+  check= true 
+  if int < 0 || int == 0 || int == 1 
+    check = false 
+    
+  end 
+  
+ (2...int).each do |div|
+   return false if int % div == 0 
+   
+ end 
+ 
+ check 
+ 
+ end 
+ 
+  
